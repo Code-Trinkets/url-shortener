4b9a90de168f04351ac5080d4451631a584cca96
Jason8908 <jasonsu894@gmail.com>
2023-07-23 17:46:35 -0400

    SHORT-5 feat: created front-end UI.

---
+++ website/src/app/components/about.js
@@ -0,0 +1,14 @@
+export default function About() {
+    return (
+        <div className="divAbout">
+            <h1>About This Project</h1>
+            <p>
+            Do you have a URL that's just too long to send to someone? Perhaps you want to 
+            share a cool link on Twitter, but the link is way too long and takes up too many 
+            of your precious characters. If this fits your description, then the JShort URL 
+            shortener is just right for you! JShort converts your long URL into a short compact URL 
+            that you can send to your friends easily.
+            </p>
+        </div>
+    )
+}
